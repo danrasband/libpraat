808d6413239a5d2f26cde35051dcde74cd1d2721
Greg Hedlund <ghedlund@mun.ca>
2014-09-24 08:37:44 -0230

    Added exports for praat version info.

--- sys/praat_version.h
+++ sys/praat_version.h
@@ -1,5 +1,27 @@
 #define PRAAT_VERSION_STR 5.3.85
+#define PRAAT_VERSION_CSTR "5.3.85"
 #define PRAAT_VERSION_NUM 5385
 #define PRAAT_YEAR 2014
 #define PRAAT_MONTH September
+#define PRAAT_MONTH_CSTR "September"
 #define PRAAT_DAY 19
+
+#ifndef PRAAT_VERSION_H
+#define PRAAT_VERSION_H
+
+#ifdef PRAAT_LIB
+// create a struct for version information
+#include "praatlib.h"
+
+struct PraatVersion {
+	const char* versionStr;
+	const int version;
+	const int year;
+	const char* month;
+	const int day;
+};
+
+PRAAT_LIB_EXPORT struct PraatVersion* praat_version();
+#endif
+
+#endif
